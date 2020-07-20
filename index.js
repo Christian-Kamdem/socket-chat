58bd499b36d98c2c680ce6e75ebc95fa9f73bde0
Christian Kamdem <christiankamdem2@gmail.com>
2020-07-20 17:23:32 +0100

    Rooms added

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
+const rooms = {};
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('disconnect', (msg) => {
+    console.log(socket.id+' is disconnected!');
   });
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+    if(rooms[msg.userId]){
+      console.log(msg.sentTo);
+      io.to(rooms[msg.userId]).emit('chat message',{
+        message:'Recu!',
+        sentAt:new Date().getTime()});
+    }else{
+      rooms[msg.userId] = socket.id;
+      console.log(msg.sentTo);
+      io.to(rooms[msg.userId]).emit('chat message',{
+        message:'Recu!',
+        sentAt:new Date().getTime()});
+    }
   });
 });
 http.listen(3000, () => {
