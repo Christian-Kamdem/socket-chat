9d980918ab86d1dbd6b811f8436abc0eed9a0cce
Christian Kamdem <christiankamdem2@gmail.com>
2020-07-24 14:10:06 +0100

    Deconnection module added

--- index.js
+++ index.js
@@ -7,19 +7,29 @@ app.get('/', (req, res) => {
 });
 io.on('connection', (socket) => {
   socket.on('disconnect', (msg) => {
-    console.log(socket.id+' is disconnected!');
+    for (const [key, value] of Object.entries(rooms)) {
+      if(socket.id == value){
+        delete rooms[key];
+        break;
+      }
+    }
   });
-  socket.on('chat message', (msg) => {console.log(msg);
-    if(rooms[msg.userId]){
+  socket.on('chat message', (msg) => {
+    if(!rooms[msg.userId]){
+      rooms[msg.userId] = socket.id;
+    }
+    //We check if the recipient is connected
+    if(rooms[msg.sendTo]){
       io.to(rooms[msg.sendTo]).emit('chat message',{
         message:msg.message,
-        from:msg.userId
+        from:msg.userId,
+        error:false
       });
     }else{
-      rooms[msg.userId] = socket.id;
-      io.to(rooms[msg.sendTo]).emit('chat message',{
-        message:msg.message,
-        from:msg.userId
+      io.to(rooms[msg.userId]).emit('chat message',{
+        message:msg.sendTo+' is disconnected',
+        from:msg.userId,
+        error:true
       });
     }
     console.log(rooms);
