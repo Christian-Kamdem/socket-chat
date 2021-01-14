6e6224b7bc60f1285c744272e2307040cc18dc8d
Christian Kamdem <christiankamdem2@gmail.com>
2021-01-14 12:33:26 +0100

    ...

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const axios = require('axios');
 const rooms = {};
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -33,6 +34,29 @@ io.on('connection', (socket) => {
         error:true
       });
     }
+//
+    let authOptions = {
+      method: 'post',
+      url: 'http://127.0.0.1/chat/dile/entryPoint.php',
+      data: JSON.stringify({
+            requestName:"load_message",
+            data:{
+              user_from:2
+            }
+          }),
+      headers: {
+       'Content-Type': 'application/json'
+      },
+      json: true
+     };
+     axios(authOptions)
+        .then((response) => {
+            console.log(response);
+             })
+        .catch((error) => {
+             console.log(error.response)
+           })
+//
   });
 });
 http.listen(3000, () => {
